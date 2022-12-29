46399c2d4a189c7ba5232479f64ed628867f96c5
cpu-cpu <69696674+cpu-cpu@users.noreply.github.com>
2022-12-29 13:06:41 -0500

    Delete Djhelper v1/Djhelper_v1_code directory

--- Djhelper v1/Djhelper_v1_code/Song_rq_bot/config.js
+++
@@ -1,6 +0,0 @@
-const config = {
-  prefix: ".",
-  devAccessIds: ["123456789"],
-}
-
-export default config;
--- Djhelper v1/Djhelper_v1_code/Song_rq_bot/events/ready.js
+++
@@ -1,7 +0,0 @@
-export const event  = {
-	name: 'ready',
-	once: true,
-	execute(client) {
-		console.log(`Ready! Logged in as ${client.user.tag}`);
-	},
-};
